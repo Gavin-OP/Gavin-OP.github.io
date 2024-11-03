dcf9c4ed5832e9c3de3a43003c5716951f013f22
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2024-11-03 23:34:28 +0800

    🦄 refactor(Home.js): comment and spaces
    - change the comment and spacing

--- src/section/Home.js
+++ src/section/Home.js
@@ -1,6 +1,4 @@
 import '../css/Home.css';
-// import { Link } from 'react-router-dom';
-// import React from 'react';
 import Navigation from '../component/common/Navbar';
 import Header from '../component/home/Header';
 import Internship from '../component/home/Internship';
@@ -14,43 +12,6 @@ import Footer from '../component/common/Footer';
 // import MyDocument from '../component/common/PdfViewer';
 
 
-// function Home() {
-//     return (
-//         <>
-//             <div className='home-header-pic-container'>
-//                 <div className='home-header-pic-mask'>
-//                     <Navigation />
-//                     <div className='text-start home-header-text'>
-//                         <h1>
-//                             <span className='home-header-name'>ZHANG Haoxiang (Gavin)</span>
-//                             <br></br>
-//                             <span className='home-header-major'>Quantitative Finance and Risk Management</span>
-//                         </h1>
-//                     </div>
-//                 </div>
-//             </div>
-//             <div className='home-body-container'>
-//                 <h1>
-//                     Personal Profile
-//                 </h1>
-//                 <p style={{ marginTop: '30px' }}>
-//                     Being a multitasker who is not only good at risk management but also
-//                     talented in time management, paperwork, communication, coding, and
-//                     various tasks, I can deal with the stress even under CUHK courses
-//                     and NTU exchange while managing 5 associations and organizing
-//                     voluntary teams simultaneously. I would like to take on the coming
-//                     challenges and innovations to embrace the goal of joining the
-//                     financial industry.
-//                 </p>
-//             </div>
-//             <Footer />
-//         </>
-//     )
-// }
-
-// export default Home;
-
-
 const Home = () => {
   return (
     <div className="home-container">
@@ -58,6 +19,7 @@ const Home = () => {
       <Header />
       <Internship />
       <Project />
+
       {/* <PdfViewer pdfPath={process.env.PUBLIC_URL + '/file/Stock_Return_Prediction_with_CNN_and_Attention_based_LSTM_models.pdf'} /> */}
       {/* <CNN_LSTM_pdf /> */}
       {/* <Fireworks /> */}
