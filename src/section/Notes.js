c8242c127bc820f5012489224b5b67363a8a9ce0
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2024-11-09 17:48:24 +0800

    🐛 refactor: remove NotesMenu component and related functionality from Notes section

--- src/section/Notes.js
+++ src/section/Notes.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { useParams, useLocation } from 'react-router-dom';
-import NotesMenu from '../component/notes/NotesMenu';
+import { useParams } from 'react-router-dom';
+// import NotesMenu from '../component/notes/NotesMenu';
 import MarkdownRenderer from '../component/notes/MarkdownRenderer';
 
 
 const Notes = () => {
     const { '*': filename } = useParams();
-    const location = useLocation();
+    // const location = useLocation();
 
-    const currentPath = location.pathname;
+    // const currentPath = location.pathname;
 
     return (
         <div className="notes-page">
-            <NotesMenu currentPath={currentPath} />
+            {/* <NotesMenu currentPath={currentPath} /> */}
             <div className="notes-content">
                 <MarkdownRenderer filename={filename} />
             </div>
         </div>
     );
 };
+
+
 
 export default Notes;
