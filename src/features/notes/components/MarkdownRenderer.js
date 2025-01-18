4ddd3cb6d7841418c82347ddfe35376485053cb2
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2025-01-18 17:03:59 +0800

    ✨ feat(MarkdownRenderer.js): format img src
    - format img source when using relative import

--- src/features/notes/components/MarkdownRenderer.js
+++ src/features/notes/components/MarkdownRenderer.js
@@ -24,147 +24,183 @@ import "../styles/MarkdownRenderer.css";
 // import 'mermaid/dist/mermaid.esm.min.mjs';
 
 const MarkdownRenderer = ({ markdownContent }) => {
-    const location = useLocation();
-    // make the checkbox can be checked or unchecked
-    // const handleCheckboxChange = (event) => {
-    //     console.log(event.target.checked);
-    // }
-    // const handleCheckboxChange = (event, index) => {
-    //     setCheckboxStates({
-    //         ...checkboxStates,
-    //         [index]: event.target.checked,
-    //     });
-    //     console.log(event.target.checked);
-    // };
-    // const handleCheckboxChange = (event, index) => {
-    //     setCheckboxStates((prevStates) => ({
-    //         ...prevStates,
-    //         [index]: !prevStates[index],
-    //     }));
-    //     console.log(event.target.checked);
-    //     console.log(index)
-    // };
+  const location = useLocation();
+  // make the checkbox can be checked or unchecked
+  // const handleCheckboxChange = (event) => {
+  //     console.log(event.target.checked);
+  // }
+  // const handleCheckboxChange = (event, index) => {
+  //     setCheckboxStates({
+  //         ...checkboxStates,
+  //         [index]: event.target.checked,
+  //     });
+  //     console.log(event.target.checked);
+  // };
+  // const handleCheckboxChange = (event, index) => {
+  //     setCheckboxStates((prevStates) => ({
+  //         ...prevStates,
+  //         [index]: !prevStates[index],
+  //     }));
+  //     console.log(event.target.checked);
+  //     console.log(index)
+  // };
 
-    const handleSmoothScroll = (id) => {
-        const element = document.getElementById(id);
-        if (element) {
+  const handleSmoothScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
       element.scrollIntoView({ behavior: "smooth" });
-        }
-    };
+    }
+  };
 
-    const getCurrentUrl = () => {
-        return `${window.location.origin}${location.pathname}${location.hash}`;
-    };
+  const getCurrentUrl = () => {
+    return `${window.location.origin}${location.pathname}${location.hash}`;
+  };
 
-    return (
-        <div className="markdown-container">
-            <div id="write">
-                <ReactMarkdown
-                    className="markdown-body"
-                    remarkPlugins={[remarkGfm, remarkMath]}
-                    rehypePlugins={[rehypeRaw, rehypeKatex]}
-                    // make the checkbox can be check or unchecked, use handleCheckBoxChange
-                    // components={{
-                    //     input({ node, ...props }) {
-                    //         console.log(node.position);
-                    //         if (props.type === 'checkbox') {
-                    //             const index = node && node.position && node.position.start ? node.position.start.offset : null;
-                    //             return <input type="checkbox" {...props} disabled={false} checked={checkboxStates[index] || false} onChange={(event) => handleCheckboxChange(event, index)}/>;
-                    //         }
-                    //         return <input {...props} />;
-                    //     },
-                    // }}
+  const resolveRelativePath = (base, relative) => {
+    const stack = base.split("/");
+    const parts = relative.split("/");
+    stack.pop();
+    for (let i = 0; i < parts.length; i++) {
+      if (parts[i] === ".") continue;
+      if (parts[i] === "..") stack.pop();
+      else stack.push(parts[i]);
+    }
+    return stack.join("/");
+  };
 
-                    // make the checkbox disable is false
-                    // components={{
-                    //     input({ node, ...props }) {
-                    //         if (props.type === 'checkbox') {
-                    //             return <input type="checkbox" {...props} disabled={false} />;
-                    //         }
-                    //         return <input {...props} />;
-                    //     },
-                    // }}
+  return (
+    <div className="markdown-container">
+      <div id="write">
+        <ReactMarkdown
+          className="markdown-body"
+          remarkPlugins={[remarkGfm, remarkMath]}
+          rehypePlugins={[rehypeRaw, rehypeKatex]}
+          // make the checkbox can be check or unchecked, use handleCheckBoxChange
+          // components={{
+          //     input({ node, ...props }) {
+          //         console.log(node.position);
+          //         if (props.type === 'checkbox') {
+          //             const index = node && node.position && node.position.start ? node.position.start.offset : null;
+          //             return <input type="checkbox" {...props} disabled={false} checked={checkboxStates[index] || false} onChange={(event) => handleCheckboxChange(event, index)}/>;
+          //         }
+          //         return <input {...props} />;
+          //     },
+          // }}
 
-                    components={{
-                        // pre({ node, className, children, ...props }) {
-                        //     if (children["type"] === "code") {
-                        //         try {
-                        //             const codeNode = children.children[0];
-                        //             console.log(codeNode)
-                        //             const match = children["props"]["className"].match(/language-(\w+)/)
-                        //             return (
+          // make the checkbox disable is false
+          // components={{
+          //     input({ node, ...props }) {
+          //         if (props.type === 'checkbox') {
+          //             return <input type="checkbox" {...props} disabled={false} />;
+          //         }
+          //         return <input {...props} />;
+          //     },
+          // }}
 
-                        //                 <pre className='md-fences'>
-                        //                     {children}
-                        //                 </pre>
-                        //             )
-                        //         } catch (e) {
-                        //             return (
-                        //                 <pre className='md-fences'>
-                        //                     {children}
-                        //                 </pre>
-                        //             )
-                        //         }
-                        //     }
-                        // },
+          components={{
+            // pre({ node, className, children, ...props }) {
+            //     if (children["type"] === "code") {
+            //         try {
+            //             const codeNode = children.children[0];
+            //             console.log(codeNode)
+            //             const match = children["props"]["className"].match(/language-(\w+)/)
+            //             return (
+
+            //                 <pre className='md-fences'>
+            //                     {children}
+            //                 </pre>
+            //             )
+            //         } catch (e) {
+            //             return (
+            //                 <pre className='md-fences'>
+            //                     {children}
+            //                 </pre>
+            //             )
+            //         }
+            //     }
+            // },
 
             // make the code block formatted
-                        pre({ node, className, children, ...props }) {
-                            const codeNode = node.children[0];
+            pre({ node, className, children, ...props }) {
+              const codeNode = node.children[0];
               const match = /language-(\w+)/.exec(
                 codeNode?.properties?.className || ""
               );
-                            return match ? (
-                                <SyntaxHighlighter
-                                    style={xcode}
-                                    language={match[1]}
-                                    PreTag="pre"
-                                    {...props}
-                                >
+              return match ? (
+                <SyntaxHighlighter
+                  style={xcode}
+                  language={match[1]}
+                  PreTag="pre"
+                  {...props}
+                >
                   {String(codeNode.children[0].value).replace(/\n$/, "")}
-                                </SyntaxHighlighter>
-                            ) : (
-                                <pre className="md-fences" {...props}>
-                                    {children}
-                                </pre>
-                            );
-                        },
+                </SyntaxHighlighter>
+              ) : (
+                <pre className="md-fences" {...props}>
+                  {children}
+                </pre>
+              );
+            },
 
             // make the footnote link clickable
-                        a({ node, ...props }) {
+            a({ node, ...props }) {
               if (
                 props.href &&
                 (props.href.startsWith("#user-content-fn-") ||
                   props.href.startsWith("#user-content-fnref-"))
               ) {
-                                const currentUrl = getCurrentUrl();
-                                return (
-                                    <a
-                                        {...props}
-                                        href={`${currentUrl}${props.href}`}
-                                        onClick={(event) => {
-                                            event.preventDefault();
-                                            handleSmoothScroll(props.href.substring(1));
-                                        }}
-                                    >
-                    {props.children.length === 0 ? "Link" : props.children}
-                                    </a>
-                                );
-                            }
-                            // return <a {...props} />;
-                            return <a {...props}>{props.children.length === 0 ? 'Link' : props.children}</a>;
-
-                        }
-
-
+                const currentUrl = getCurrentUrl();
+                return (
+                  <a
+                    {...props}
+                    href={`${currentUrl}${props.href}`}
+                    onClick={(event) => {
+                      event.preventDefault();
+                      handleSmoothScroll(props.href.substring(1));
                     }}
+                  >
+                    {props.children.length === 0 ? "Link" : props.children}
+                  </a>
+                );
+              }
+              return (
+                <a {...props}>
+                  {props.children.length === 0 ? "Link" : props.children}
+                </a>
+              );
+            },
 
-                >
+            // make the image path correct for relative import
+            img({ node, ...props }) {
+              if (
+                props.src &&
+                (props.src.startsWith("/") || props.src.startsWith("."))
+              ) {
+                // const hashPath = window.location.hash.replace("#", "public");
+                // if (props.src.startsWith("/")) {
+                //   const src = `${hashPath}${props.src}`;
+                //   return <img {...props} src={src} />;
+                // } else if (props.src.startsWith(".")) {
+                //   const src = `${hashPath}/${props.src}`;
+                //   return <img {...props} src={src} />;
+                // }
+                const src = resolveRelativePath(
+                  window.location.hash.replace("#", "public"),
+                  props.src
+                );
+                return <img {...props} src={src} />;
+              }
+              return <img {...props} />;
+            },
+
+
+          }}
+        >
           {markdownContent}
-                </ReactMarkdown>
-            </div>
-        </div>
-    );
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
 };
 
 export default MarkdownRenderer;
