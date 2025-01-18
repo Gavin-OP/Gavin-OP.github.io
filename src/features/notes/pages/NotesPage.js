02c1922b125000f1e1c80ae59af10285fc9b7c70
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2025-01-18 17:04:52 +0800

    ✨ feat(NotesPage.js): new render logic
    - add the fetch content logic here
    - use new MarkdownRenderer
    - remove menu for now

--- src/features/notes/pages/NotesPage.js
+++ src/features/notes/pages/NotesPage.js
@@ -1,43 +1,43 @@
-import { React, useEffect, useState } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
-import NotesMenu from '../components/NotesMenu';
-import MarkdownRenderer from '../components/MarkdownRenderer';
-
+import React, { useEffect, useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
+import NotesMenu from "../components/NotesMenu";
+import MarkdownRenderer from "../components/MarkdownRenderer";
+import { GitHubService } from "../../../services/GitHub";
+import { GITHUB_CONFIG } from "../../../utils/Constants";
 
 const NotesPage = () => {
-    const { '*': filename } = useParams();
-    const location = useLocation();
-    const [exists, setExists] = useState(true);
+  const { "*": filename } = useParams();
+  const [content, setContent] = useState("");
 
-    const currentPath = location.pathname;
+  useEffect(() => {
+    const fetchContent = async () => {
+      const github = new GitHubService(
+        GITHUB_CONFIG.owner,
+        GITHUB_CONFIG.repo,
+        GITHUB_CONFIG.branch,
+        GITHUB_CONFIG.basePath,
+        process.env.REACT_APP_GITHUB_TOKEN
+      );
+      const filePath = `notes/${filename || "disclaimer"}.md`;
+      try {
+        const fileContent = await github.getFileContent(filePath);
+        setContent(fileContent);
+      } catch (err) {
+        console.error("Error fetching content:", err);
+      }
+    };
 
-    useEffect(() => {
-        // Check if file exists
-        fetch(`/notes/${filename || 'disclaimer'}`)
-            .then((response) => response.text())
-            .then((text) => {
-                // console.log(text);
-                if (text.includes("<!DOCTYPE html>")) {
-                    setExists(false);
-                }
-                setExists(true);
-            })
-            .catch(() => setExists(false));
-    }, [filename]);
+    fetchContent();
+  }, [filename]);
 
-    console.log("filename: ", filename, "exists: ", exists, 'currentPath: ', currentPath);
-
-
-    return (
-        <div className="notes-page">
-            <NotesMenu currentPath={currentPath} />
-            <div className="notes-content">
-                <MarkdownRenderer filename={exists ? filename : 'disclaimer'} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="notes-page">
+      {/* <NotesMenu currentPath={filename} /> */}
+      <div className="notes-content">
+        <MarkdownRenderer markdownContent={content} />
+      </div>
+    </div>
+  );
 };
-
-
 
 export default NotesPage;
